a73401bbb8105edaee741c30395b3e4fe162dc55
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-12-13 22:46:58 +0900

    adjust gap between features

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -44,11 +44,11 @@ const features = [
 export default function Feature() {
   return (
     <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto flex max-w-7xl flex-col gap-20 px-6 lg:px-8">
         {features.map((feature) => (
           <div
             key={feature.name}
-            className="mx-auto mb-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2"
+            className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2"
           >
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
