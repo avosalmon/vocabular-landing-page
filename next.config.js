73f502f965a474403d2db5576dcbf0d6ff3457a5
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2024-04-14 12:05:59 +0900

    add mdx support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const withMDX = require("@next/mdx")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
