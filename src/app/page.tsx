d367d66f279339c06dc198a049ac6a59ede23680
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2024-04-20 11:23:00 +0900

    extract header client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,135 +1,22 @@
-"use client";
-
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Feature from "@/components/Feature";
-import Pricing from "@/components/Pricing";
-import Footer from "@/components/Footer";
 import FAQ from "@/components/FAQ";
+import Feature from "@/components/Feature";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { Metadata } from "next";
 import Link from "next/link";
-import Logo from "@/components/Logo";
-import Head from "next/head";
 
-const navigation = [
-  { name: "Translate", href: "#translate" },
-  { name: "Lookup", href: "#lookup" },
-  { name: "Write Better", href: "#write-better" },
-  { name: "Compare", href: "#compare" },
-];
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "/",
+  },
+};
 
 const signupUrl = "https://go.vocabular.ai/register";
 
 export default function Home() {
-  const router = useRouter();
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <div className="bg-white">
-      <Head>
-        <link
-          rel="canonical"
-          href="https://www.vocabular.ai/"
-          key="canonical"
-        />
-      </Head>
-
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="flex items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="./" className="-m-1.5 p-1.5">
-              <span className="sr-only">Vocabular</span>
-              <Logo className="h-6 w-auto" />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link
-              href={signupUrl}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Sign up <span aria-hidden="true">&rarr;</span>
-            </Link>
-          </div>
-        </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Vocabular</span>
-                <Logo className="h-6 w-auto" />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setMobileMenuOpen(false);
-                        router.push(item.href);
-                      }}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-                <div className="py-6">
-                  <Link
-                    href={signupUrl}
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Sign up
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
-      </header>
-
+      <Header />
       <div className="relative isolate pt-14">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
